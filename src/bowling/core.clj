13e1576ca5767a1b5ae2a7ed9ed78a1479ebea08
Erik Assum <erik@assum.net>
2015-08-10 21:03:49 +0200

    New better version
    Removed code for rolling, but added better scoring

--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -1,90 +1,42 @@
 (ns bowling.core)
 
-(defn rolled? [v]
-  (not (= -1 v)))
+(def game [[1 4]
+           [4 5]
+           [6 4]
+           [5 5]
+           [10]
+           [0 1]
+           [7 3]
+           [6 4]
+           [10]
+           [2 8 6]])
+
+(def full-frame 10)
+
+(defn true-score [frame]
+  (reduce + (take 2 frame)))
 
 (defn strike? [frame]
-  (= 10 (first frame)))
+  (= 1 (count frame)))
 
-(defn new-frame? 
-  "Should we update the current frame, or should we create a new one?"
-  [frame]
-    (or (strike? frame) (rolled? (second frame))))
+(defn spare? [frame]
+  (and (= full-frame (true-score frame)) (not (strike? frame))))
 
-(defn roll 
-  "If frames is empty, we add a new frame,
-   otherwise we either update the first frame or create
-   a new one, returns the updated frames"
-  [frames pins]
-  (if (empty? frames)
-    (conj frames [pins -1])
-    (let [frame (first frames)
-          f (first frame)]
-      (if (new-frame? frame)
-        (conj frames [pins -1])
-        (cons [f pins] (rest frames))))))
+(defn strike-bonus [[next second & _]]
+  (if-not (strike? next)
+    (true-score next)
+    (+ full-frame (first second))))
 
-(defn frame-score
-  "Give the score of a frame without bonus"
-  [frame]
-  (->> frame
-       (filter rolled?)
-       (reduce +)))
+(defn bonus [current rest]
+  (cond (strike? current) (strike-bonus rest)
+        (spare? current) (first (first rest))
+        :else 0))
 
-(defn frame-total [frame bonus]
-  "Calculates the total of a frame incl. bonus"
-  (let [score (frame-score frame)]
-   (cond
-     (< score 10) score 
-     (strike? frame) (apply + 10 bonus)
-     :else  (+ 10 (first bonus)))))
+(defn score-frames [[current & rest]]
+  (let [frame-score (reduce + current)]
+    (if-not rest
+      (list frame-score)
+      (cons (+ frame-score (bonus current rest)) (score-frames rest)))))
 
-(defn bonus-rolls
-  "Gets the next two rolls from the list of frames"
-  [frames]
-  (when (seq frames)
-    (->> frames
-         (flatten)
-         (filter rolled?)
-         (take 2))))
-
-(defn score
-  "Calculates the score of a game, starting from the back, having the possible bonus rolls in bonus,
-   will call itself recursivly until the first
-   frame start by calling (score frames '(0 0))
-   If not called with bonus, it will calculate its own"
-  ([frames]
-     (let [frame-count (count frames)]
-       (cond (= 11 frame-count) (score (rest frames) (bonus-rolls frames))
-             (= 12 frame-count) (score (rest (rest frames)) (bonus-rolls frames))
-             :else  (score frames [0 0]))))
-  ([frames bonus]
-     (if (seq frames)
-         (+ (frame-total (first frames) bonus)
-            (score (rest frames) (bonus-rolls frames)))
-       0)))
-  
-(comment (-> (-> '() 
-         (roll 9)
-         (roll 0)
-         (roll 3)
-         (roll 7)			    
-         (roll 6)
-         (roll 1)
-         (roll 3)			    
-         (roll 7)
-         (roll 8)
-         (roll 1)
-         (roll 5)
-         (roll 5)
-         (roll 0)
-         (roll 10)
-         (roll 8)
-         (roll 0)
-         (roll 7)
-         (roll 3)
-         (roll 8)
-         (roll 2)
-         (roll 8))
-         (score )))
-;=> 131
+(reductions + (score-frames game))
+;; => (5 14 29 49 60 61 77 97 117 133)
